b715f0732caebcc018eb76f3dbcf7ff52481d406
Sajid Rafique <sajid.rafique@codedistrict.com>
2022-11-02 16:58:20 +0500

    unit test case fixing for service

--- tests/TreeGetApiTest.php
+++ tests/TreeGetApiTest.php
@@ -8,7 +8,6 @@ class TreeGetApiTest extends TestCase
      */
     public function testTreeGetApiWithEmptyFormData()
     {
-        print sprintf("Test with empty form data");
         $response = $this->call('POST', '/api/v1/tree/get', []);
         $this->assertEquals(422, $response->status());
     }
@@ -19,7 +18,6 @@ class TreeGetApiTest extends TestCase
      */
     public function testTreeGetApiWithEmptyValues()
     {
-        print sprintf("Test with empty values");
         $response = $this->call('POST', '/api/v1/tree/get', ['topic_num'=>'','asofdate'=>'','algorithm'=>'','update_all'=>'']);
         $this->assertEquals(422, $response->status());
     }
@@ -29,7 +27,6 @@ class TreeGetApiTest extends TestCase
      */
     public function testStoreApiWithCorrectValues()
     {
-        print sprintf("Test with correct values");
         $response = $this->call('POST', '/api/v1/tree/get', ['topic_num'=>238,'asofdate'=>time(),'algorithm'=>'blind_popularity','update_all'=>0]);
         $this->assertEquals(200, $response->status());
     }
