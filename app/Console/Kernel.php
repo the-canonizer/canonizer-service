7b9d0da40fdc11a1cf0b5e1d4f333839bceabb5f
M-Darab <m.darabkhan@codedistrict.com>
2023-01-09 16:44:24 +0500

    command created for delete non latest trees

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 use App\Console\Commands\CreateTopicTreeCommand;
 use App\Console\Commands\RemoveDuplicateTrees;
+use App\Console\Commands\RemoveNonLatestTreesCommand;
 use App\Console\Commands\TruncateOldTrees;
 
 class Kernel extends ConsoleKernel
@@ -19,6 +20,7 @@ class Kernel extends ConsoleKernel
         CreateTopicTreeCommand::class,
         TruncateOldTrees::class,
         RemoveDuplicateTrees::class,
+        RemoveNonLatestTreesCommand::class,
     ];
 
     /**
