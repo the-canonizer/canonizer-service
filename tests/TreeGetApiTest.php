e32fa459af2a92dabf210d9346489c0202fb2251
M-Darab <m.darabkhan@codedistrict.com>
2024-06-11 16:06:30 +0500

    feature/CAN-SERVICE-1020: GetTopicsAPI Test for view count

--- tests/TreeGetApiTest.php
+++ tests/TreeGetApiTest.php
@@ -12,13 +12,13 @@ class TreeGetApiTest extends TestCase
         $this->assertEquals(422, $response->status());
     }
 
-     /**
+    /**
      * Check Api with empty values
      * validation
      */
     public function testTreeGetApiWithEmptyValues()
     {
-        $response = $this->call('POST', '/api/v1/tree/get', ['topic_num'=>'','asofdate'=>'','algorithm'=>'','update_all'=>'']);
+        $response = $this->call('POST', '/api/v1/tree/get', ['topic_num' => '', 'asofdate' => '', 'algorithm' => '', 'update_all' => '']);
         $this->assertEquals(422, $response->status());
     }
 
@@ -27,7 +27,42 @@ class TreeGetApiTest extends TestCase
      */
     public function testStoreApiWithCorrectValues()
     {
-        $response = $this->call('POST', '/api/v1/tree/get', ['topic_num'=>238,'asofdate'=>time(),'algorithm'=>'blind_popularity','update_all'=>0]);
+        $response = $this->call('POST', '/api/v1/tree/get', ['topic_num' => 238, 'asofdate' => time(), 'algorithm' => 'blind_popularity', 'update_all' => 0, "view" => "\$QWZhYWNiVmhtMTE2ekt3Vg\$iIc0UGbTCDIgCXXwpHnzXA"]);
         $this->assertEquals(200, $response->status());
+    }
+
+    public function testWithCorrectValuesForValidResponseStructure()
+    {
+        $response = $this->call('POST', '/api/v1/tree/get', ['topic_num' => 238, 'asofdate' => time(), 'algorithm' => 'blind_popularity', 'update_all' => 0]);
+        $response->assertJsonStructure([
+            "code",
+            "success",
+            'data' => [
+                0 => [
+                    '1' => [
+                        "topic_id",
+                        "camp_id",
+                        "title",
+                        "review_title",
+                        "link",
+                        "review_link",
+                        "score",
+                        "full_score",
+                        "submitter_nick_id",
+                        "created_date",
+                        "is_valid_as_of_time",
+                        "is_disabled",
+                        "is_one_level",
+                        "is_archive",
+                        "direct_archive",
+                        "subscribed_users",
+                        "support_tree",
+                        "children",
+                        "collapsedTreeCampIds",
+                        "camp_views"
+                    ]
+                ]
+            ],
+        ]);
     }
 }
