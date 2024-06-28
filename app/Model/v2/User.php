d9355d8fd742e2966874a04ac9b80f318ae3dc05
M-Darab <m.darabkhan@codedistrict.com>
2024-06-28 15:46:21 +0500

    feature/CAN-SERVICE-1060: Browse page API Update

--- app/Model/v2/User.php
+++ app/Model/v2/User.php
@@ -15,7 +15,7 @@ class User extends Model
      * @var array
      */
     protected $fillable = [
-        'first_name','last_name','middle_name', 'email', 'password','language','status','otp','provider','provider_id'
+        'first_name', 'last_name', 'middle_name', 'email', 'password', 'language', 'status', 'otp', 'provider', 'provider_id'
     ];
 
     /**
@@ -26,23 +26,22 @@ class User extends Model
     protected $hidden = [
         'password', 'remember_token',
     ];
-    
+
     // Define the accessor for the profile_picture_path attribute
     public function getProfilePicturePathAttribute($value)
     {
-        if (is_null($value)) {
-            return null;
-        }
-        return urldecode(env('AWS_PUBLIC_URL') . '/' . $value);
+        return !is_null($value) ? urldecode(env('AWS_PUBLIC_URL') . '/' . $value) : null;
     }
 
-    public static function getByEmail($email){        
+    public static function getByEmail($email)
+    {
         $user = User::where('email', $email)->first();
-       return !empty($user) ? $user : false;
+        return !empty($user) ? $user : false;
     }
 
-    public function getNameAttribute(){
-       return ucwords ($this->first_name.' '.$this->last_name);
+    public function getNameAttribute()
+    {
+        return ucwords($this->first_name . ' ' . $this->last_name);
     }
 
     /**
@@ -50,7 +49,8 @@ class User extends Model
      * @param interger $id
      * @return User 
      */
-    public static function getById($id) {
+    public static function getById($id)
+    {
         return User::where('id', $id)->first();
     }
 }
